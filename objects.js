b01d3ace97092ebc68965aa5f8f1b78759c8eb7c
Joshua Suskin <jsuskin85@gmail.com>
2018-06-09 07:33:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign({}, playlist, {[artistName]: songTitle});
   return playlist;
 }
+
+function removeFromPlaylist(playlist,artistName) {
+  delete playlist.artistName;
+  return playlist;
+}
